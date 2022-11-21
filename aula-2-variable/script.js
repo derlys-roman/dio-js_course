43992fa831520ad0c39007a644c3d4a63b5d5bc2
Derlys <derlysr6@gmail.com>
2022-11-21 06:29:59 -0300

    First commit

---
+++ aula-2-variable/script.js
@@ -0,0 +1,11 @@
+//variables
+//global and mutable variables is used var 
+var anyVariable = "anyVariable"
+console.log(anyVariable)
+//local and mutable variables is used let
+let anyLocalVariable = "anyLocalVariable"
+console.log(anyLocalVariable)
+//local and immutable variables is used const
+const anyLocalVariableConst = "anyLocalVariableConst"
+console.log(anyLocalVariableConst)
+//just this today
